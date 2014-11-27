deaa1f815634a35a02f918271ecde99caa1a8e91
Dennis Zinzi <d.zinzi@ncl.ac.uk>
2014-11-27 18:37:38 +0000

    First Commit

---
+++ tddCoursework/src/com/example/tddCoursework/ExampleLoggingTest.java
@@ -0,0 +1,24 @@
+package com.example.tddCoursework;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleLoggingTest extends AbstractLoggingJUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test3(){
+		assertEquals(4,2+2);
+	}
+
+}
